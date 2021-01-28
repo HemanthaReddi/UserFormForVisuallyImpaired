f12c815f2b5ed1247a082a1830e5e67ee7a33f4c
Hemantha Reddi <hemantha.reddi@gmail.com>
2021-01-28 23:00:24 +0530

    Edited as per Accessibility rules

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
 
+document.onmouseover = function(e) {
+    console.log(e.target.id);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   var checkbox = document.querySelector('input[type="checkbox"]');
 
   checkbox.addEventListener('change', function () {
     if (!checkbox.checked) {
       document.getElementById("accessibilityEnabled").innerHTML="Accessibilty disabled";
+	  var accessibilityEnabled = false;
     } 
 	else {
       document.getElementById("accessibilityEnabled").innerHTML="Accessibilty Enabled";
-
+	  var accessibilityEnabled = true;
     }
   });
 });
 
 function textToAudio() {
-                let msg = document.getElementById("fname").value;
-                
+                let msg = document.getElementById("fname").innerHTML;
+                console.log(msg);
+				console.log("hello");
                 let speech = new SpeechSynthesisUtterance();
                 speech.lang = "en-US";
                 
@@ -24,6 +30,7 @@ function textToAudio() {
                 speech.rate = 1;
                 speech.pitch = 1;
                 
+				
 				window.speechSynthesis.cancel();
                 window.speechSynthesis.speak(speech);
 				
